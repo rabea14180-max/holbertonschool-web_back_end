ef2efc3bf9c9639633570fb88371dd6932bb01d4
rabea14180-max <rabea14180@gmail.com>
2026-04-12 16:58:00 +0300

    Refactor safe_first_element function for conciseness

--- python_variable_annotations/100-safe_first_element.py
+++ python_variable_annotations/100-safe_first_element.py
@@ -1,12 +1,6 @@
 #!/usr/bin/env python3
-"""Module for safe_first_element function"""
-
 from typing import Sequence, Any, Optional
 
 
 def safe_first_element(lst: Sequence[Any]) -> Optional[Any]:
-    if lst:
-        return lst[0]
-    else:
-        return None
-    
+    return lst[0] if lst else None
