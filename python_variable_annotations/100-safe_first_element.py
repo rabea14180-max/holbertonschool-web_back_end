b4971b9c0f6724ebf5d478e9e3c751a8e4f5bccb
rabea14180-max <rabea14180@gmail.com>
2026-04-12 17:06:19 +0300

    Add docstring to safe_first_element function for improved clarity

--- python_variable_annotations/100-safe_first_element.py
+++ python_variable_annotations/100-safe_first_element.py
@@ -1,6 +1,10 @@
 #!/usr/bin/env python3
+
+"""10. Duck typing - First element of a sequence"""
+
 from typing import Sequence, Any, Optional
 
 
 def safe_first_element(lst: Sequence[Any]) -> Optional[Any]:
+    """return first element of lst if not empty, else None"""
     return lst[0] if lst else None
