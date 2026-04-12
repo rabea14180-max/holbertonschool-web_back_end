8dde301ab4fc462d896dc663cb29d5588e1880ec
rabea14180-max <rabea14180@gmail.com>
2026-04-12 16:48:03 +0300

    Refactor docstrings in async_generator for clarity and consistency

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -1,7 +1,5 @@
 #!/usr/bin/env python3
-"""
-Module for async_generator coroutine.
-"""
+"""This module provides an asynchronous generator."""
 
 import asyncio
 import random
@@ -9,12 +7,7 @@ from typing import Generator
 
 
 async def async_generator() -> Generator[float, None, None]:
-    """
-    Asynchronously generate 10 random numbers between 0 and 10.
-    Yields:
-        A random float between 0 and 10.
-    """
-    for i in range(10):
+    """Yields 10 random numbers between 0 and 10 with a delay of 1 second."""
+    for _ in range(10):
         await asyncio.sleep(1)
         yield random.uniform(0, 10)
-        
