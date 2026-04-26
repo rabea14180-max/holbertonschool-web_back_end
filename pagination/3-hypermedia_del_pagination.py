5a20e69af553bcaaac79bb29a370e1f4d9bac3c1
rabea14180-max <rabea14180@gmail.com>
2026-04-26 23:01:09 +0300

    feat: add deletion-resilient pagination

---
+++ pagination/3-hypermedia_del_pagination.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python3
+"""
+Deletion-resilient hypermedia pagination
+"""
+
+import csv
+from typing import List, Dict
+
+
+class Server:
+    """Server class to paginate a database of popular baby names."""
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        """Initialize the server."""
+        self.__dataset = None
+        self.__indexed_dataset = None
+
+    def dataset(self) -> List[List]:
+        """Return the cached dataset."""
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
+        return self.__dataset
+
+    def indexed_dataset(self) -> Dict[int, List]:
+        """Return the dataset indexed by sorting position, starting at 0."""
+        if self.__indexed_dataset is None:
+            dataset = self.dataset()
+            truncated_dataset = dataset[:1000]
+            self.__indexed_dataset = {
+                i: truncated_dataset[i] for i in range(len(truncated_dataset))
+            }
+        return self.__indexed_dataset
+
+    def get_hyper_index(self, index: int = None, page_size: int = 10) -> Dict:
+        """Return a deletion-resilient hypermedia pagination dictionary."""
+        indexed_dataset = self.indexed_dataset()
+
+        if index is None:
+            index = 0
+
+        assert 0 <= index < len(self.dataset())
+
+        data = []
+        current_index = index
+
+        while len(data) < page_size and current_index < len(self.dataset()):
+            if current_index in indexed_dataset:
+                data.append(indexed_dataset[current_index])
+            current_index += 1
+
+        return {
+            "index": index,
+            "next_index": current_index,
+            "page_size": len(data),
+            "data": data
+        }
