c1bae018ff65e449c29996c61d47dd73bf623542
rabea14180-max <rabea14180@gmail.com>
2026-04-27 12:46:57 +0300

    feat: implement function to return students sorted by average score

---
+++ NoSQL/101-students.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+"""
+returns all students sorted by average score
+"""
+
+
+def top_students(mongo_collection):
+    """Returns students sorted by average score"""
+
+    pipeline = [
+        {
+            "$addFields": {
+                "averageScore": {"$avg": "$topics.score"}
+            }
+        },
+        {
+            "$sort": {"averageScore": -1}
+        }
+    ]
+
+    return list(mongo_collection.aggregate(pipeline))
