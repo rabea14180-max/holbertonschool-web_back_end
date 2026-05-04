5764209d767ee7f4e3df57890acda1ea067b5a9f
rabea14180-max <rabea14180@gmail.com>
2026-05-04 15:11:59 +0300

    feat: add Car class with constructor, accessors for brand, motor, and color, and a clone method

---
+++ ES6_classes/100-evcar.js
@@ -0,0 +1,24 @@
+export default class Car {
+  constructor(brand, motor, color) {
+    this._brand = brand;
+    this._motor = motor;
+    this._color = color;
+  }
+
+  get brand() {
+    return this._brand;
+  }
+
+  get motor() {
+    return this._motor;
+  }
+
+  get color() {
+    return this._color;
+  }
+
+  cloneCar() {
+    const Species = this.constructor[Symbol.species] || this.constructor;
+    return new Species(this._brand, this._motor, this._color);
+  }
+}
