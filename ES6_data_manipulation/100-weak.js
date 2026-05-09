1691d6b18e15e036daa627134f294cf61c8a1c10
rabea14180-max <rabea14180@gmail.com>
2026-05-10 00:54:23 +0300

    feat: implement queryAPI function to track endpoint load using a WeakMap

---
+++ ES6_data_manipulation/100-weak.js
@@ -0,0 +1,13 @@
+const weakMap = new WeakMap();
+
+const queryAPI = (endpoint) => {
+  const count = weakMap.get(endpoint) || 0;
+
+  if (count >= 5) {
+    throw new Error('Endpoint load is high');
+  }
+
+  weakMap.set(endpoint, count + 1);
+};
+
+export { queryAPI, weakMap };
