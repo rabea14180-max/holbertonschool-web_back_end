d0329851a1207b8709540ab1d1f5f33b44f14dd9
rabea14180-max <rabea14180@gmail.com>
2026-05-10 01:14:10 +0300

    feat: implement asyncUploadUser function to handle user and photo uploads

---
+++ ES6_promise/100-await.js
@@ -0,0 +1,19 @@
+import uploadPhoto from './utils';
+import createUser from './utils';
+
+export default async function asyncUploadUser() {
+  try {
+    const photo = await uploadPhoto();
+    const user = await createUser();
+
+    return {
+      photo,
+      user,
+    };
+  } catch (error) {
+    return {
+      photo: null,
+      user: null,
+    };
+  }
+}
