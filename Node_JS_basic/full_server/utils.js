99bad6e7b6f76ef8f0b2bb0ebe7dd6336b9628c1
Rabea Thabit <rabea14180@gmail.com>
2026-05-17 16:00:21 +0300

    Add readDatabase function to read and parse data

---
+++ Node_JS_basic/full_server/utils.js
@@ -0,0 +1,27 @@
+const fs = require('fs');
+
+function readDatabase(filePath) {
+  return new Promise((resolve, reject) => {
+    fs.readFile(filePath, 'utf8', (err, data) => {
+      if (err) {
+        reject(new Error('Cannot load the database'));
+        return;
+      }
+
+      const lines = data.split('\n').filter((line) => line.trim() !== '').slice(1);
+      const fields = {};
+
+      lines.forEach((line) => {
+        const parts = line.split(',');
+        const firstName = parts[0];
+        const field = parts[3];
+        if (!fields[field]) fields[field] = [];
+        fields[field].push(firstName);
+      });
+
+      resolve(fields);
+    });
+  });
+}
+
+module.exports = readDatabase;
